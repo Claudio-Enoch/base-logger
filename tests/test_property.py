ec3c86cc2bf809731b1294b900cd2d7bb5779b48
Elie S <1elie21356@gmail.com>
2020-08-14 16:56:48 -0600

    smh... this makes me want to quit python

--- tests/test_property.py
+++ tests/test_property.py
@@ -1,6 +1,22 @@
+from base_logger import BaseLogger, log_method
+
+
 def test_property(capfd, obj):
     return_value = obj.property
 
     stdout, stderr = capfd.readouterr()
     assert f"{obj.__class__.__name__}.property" in stdout
     assert return_value in stdout
+
+
+def test_nested_property(capfd):
+    class Foo:
+        def __init__(self):
+            self.prop = property(lambda x: "str")
+            setattr(self, "prop", log_method(getattr(self, "prop")))
+
+    obj = Foo()
+    return_value = obj.prop().fget(1)
+
+    stdout, stderr = capfd.readouterr()
+    assert f"lambda" in stdout
